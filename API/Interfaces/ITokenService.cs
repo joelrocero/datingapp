45e2e47fad5ba88f9b3ff08d4a81f1609e58fb96
joelrocero <joelrocero19@gmail.com>
2021-11-28 21:37:35 +0800

    InitialCmmit

---
+++ API/Interfaces/ITokenService.cs
@@ -0,0 +1,15 @@
+﻿using API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.interfaces
+{
+    public interface ITokenService
+    {
+        Task<string> CreateToken(AppUser user);
+
+
+    }
+}
